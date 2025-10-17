f3dea7ce68ea9ec84c9f89e18b747bc49b7472db
Kota-35 <ishikuro6.2@gmail.com>
2025-10-17 21:04:11 +0900

    feat: SE-BT3の解答を完成させる

--- bt3/src/main/java/bt3/BasicTask3.java
+++ bt3/src/main/java/bt3/BasicTask3.java
@@ -1,5 +1,6 @@
 package bt3;
 import java.util.Arrays;
+
 public class BasicTask3 {
     //numbersの要素が素数か否かを判定し，素数であればprimesの要素とする
     //numbersの要素を変えても対応できるプログラムにしてください
@@ -9,9 +10,47 @@ public class BasicTask3 {
     public static void main(String[] args) {
         preprocess(args); //ここは無視でOK
         //ここから
-        primes = new int[numbers.length]; //ダミーなので消してよい
+
+        primes = getPrimeNumberArray(numbers);
+
         //ここまで
         System.out.println(Arrays.toString(primes));
+    }
+
+    /**
+     * テストコードでテストしやすくするために関数に分離
+     */
+    public static int[] getPrimeNumberArray(int[] numberArray) {
+        return Arrays.stream(numberArray).filter(i -> isPrime(i)).toArray();
+    }
+
+    /**
+     * 引数n が 素数かどうか判定する
+     */
+    public static boolean isPrime(int n) {
+        // 2は唯一の偶数の素数
+        if (n == 2) {
+            return true;
+        } 
+        // 2以外の偶数は素数ではない
+        else if (n % 2 == 0) {
+            return false;
+        }
+
+        // 奇数のみをチェック（3から開始）
+        int i = 3;
+        // 平方根までチェックすれば十分(の合成数は全て以下である。)
+        while (i <= Math.sqrt(n)) {
+            // 割り切れる数があれば合成数
+            if (n % i == 0) {
+                return false;
+            } 
+            // 次の奇数に進む（偶数は既に除外済み）
+            i+=2;
+        }
+
+        // すべてのチェックを通過したら素数
+        return true;
     }
 
     // このメソッドは無視でOK
--- bt3/src/test/java/bt3/BasicTask3Test.java
+++ bt3/src/test/java/bt3/BasicTask3Test.java
@@ -1,5 +1,104 @@
 package bt3;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class BasicTask3Test {
     
+    @Test
+    public void isPrimeTest1() {
+        int n = 2;
+
+        boolean actual = BasicTask3.isPrime(n);
+
+        assertEquals(true, actual);
+    }
+
+    @Test
+    public void isPrimeTest2() {
+        int n = 10;
+
+        boolean actual = BasicTask3.isPrime(n);
+
+        assertEquals(false, actual);
+    }
+
+    @Test
+    public void isPrimeTest3() {
+        int n = 110;
+
+        boolean actual = BasicTask3.isPrime(n);
+
+        assertEquals(false, actual);
+    }
+
+    @Test
+    public void isPrimeTest4() {
+        int n = 151;
+
+        boolean actual = BasicTask3.isPrime(n);
+
+        assertEquals(true, actual);
+    }
+
+    @Test
+    public void isPrimeTest5() {
+        int n = 9;
+
+        boolean actual = BasicTask3.isPrime(n);
+
+        assertEquals(false, actual);
+    }
+
+    @Test
+    public void getPrimeNumberArrayTest1() {
+        int[] numbers = {7, 8, 9, 10, 11};
+
+        int[] expected = {7, 11};
+        int[] actual = BasicTask3.getPrimeNumberArray(numbers);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void getPrimeNumberArrayTest2() {
+        int[] numbers = {42, 17, 23, 4, 29, 30, 19, 20};
+
+        int[] expected = {17, 23, 29, 19};
+        int[] actual = BasicTask3.getPrimeNumberArray(numbers);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void getPrimeNumberArrayTest3() {
+        int[] numbers = {121, 2, 97, 121, 89, 90, 97, 1000, 3};
+
+        int[] expected = {2, 97, 89, 97, 3};
+        int[] actual = BasicTask3.getPrimeNumberArray(numbers);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void getPrimeNumberArrayTest4() {
+        int[] numbers = {250, 251, 37, 38, 997, 998, 999, 1009};
+
+        int[] expected = {251, 37, 997, 1009};
+        int[] actual = BasicTask3.getPrimeNumberArray(numbers);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void getPrimeNumberArrayTest5() {
+        int[] numbers = {2, 49, 83, 64, 127, 143, 169, 173};
+
+        int[] expected = {2, 83, 127, 173};
+        int[] actual = BasicTask3.getPrimeNumberArray(numbers);
+
+        assertArrayEquals(expected, actual);
+    }
 }
