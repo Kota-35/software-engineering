a4ca5d0028bfa83edfc1d8848f0b0b9405d3fda7
Kota-35 <ishikuro6.2@gmail.com>
2025-10-17 14:21:48 +0900

    feat: Mavenによるプロジェクトの作成

---
+++ bt1/src/main/java/bt1/BasicTask1.java
@@ -0,0 +1,35 @@
+package bt1;
+
+public class BasicTask1 {
+    public static int max = 100; // 2^31-1 >= max >= 3
+
+    public static void main(String[] args) {
+        preprocess(args); // ここは無視でOK
+        boolean result = isRem1();
+        System.out.print("奇数の二乗を8で割ると常に余りは1");
+        if(result) {
+            System.out.println("である");
+        } else {
+            System.out.println("ではない");
+        }
+    }
+
+    /**
+     * isRem1メソッドの仕様：
+     * ・区間[3, max]にある各奇数の二乗数を8で割ると1余るかを判定する．
+     * ・常に1余るようであれば，resultを変更しない．
+     * ・いずれか1つでも余りが1にならなければ，resultにfalseを代入する．
+     */
+    public static boolean isRem1() {
+        boolean result = true;
+        // この中身を実装する
+        return result;
+    }
+
+    // このメソッドは無視してOK
+    public static void preprocess(String[] args) {
+        if(args.length > 0) {
+            max = Integer.parseInt(args[0]);
+        }
+    }
+}
---
+++ bt1/src/test/java/bt1/BasicTask1.java
@@ -0,0 +1,14 @@
+package bt1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BasicTask1 {
+    
+    @Test
+    void isRem1Test() {
+        assertEquals(1, 2);
+    }
+}
+ 
