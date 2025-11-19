edb1458f03995e31e4f00133b88ce5836b77f394
Kota-35 <ishikuro6.2@gmail.com>
2025-11-19 17:25:42 +0900

    feat: formatterの導入

--- at1/src/main/java/at1/ApplicationTask1.java
+++ at1/src/main/java/at1/ApplicationTask1.java
@@ -1,7 +1,8 @@
 package at1;
 
 public class ApplicationTask1 {
-    public static void main(String[] args) {
-        new TodoManager().run();
-    }
+  public static void main(String[] args) {
+    new TodoManager().run();
+  }
+
 }
--- at1/src/main/java/at1/service/TaskService.java
+++ at1/src/main/java/at1/service/TaskService.java
@@ -1,31 +1,30 @@
 package at1.service;
 
-import java.io.IOException;
-import java.util.List;
-
 import at1.domain.Task;
 import at1.domain.TaskRepository;
 import at1.infrastructure.FileTaskStorage;
+import java.io.IOException;
+import java.util.List;
 
 public class TaskService {
-    private TaskRepository repository;
-    private FileTaskStorage storage;
+  private TaskRepository repository;
+  private FileTaskStorage storage;
 
-    public TaskService(TaskRepository repository, FileTaskStorage storage) {
-        this.repository = repository;
-        this.storage = storage;
-    }
+  public TaskService(TaskRepository repository, FileTaskStorage storage) {
+    this.repository = repository;
+    this.storage = storage;
+  }
 
-    // タスクをストレージに保存
-    public void save() throws IOException {
-        List<Task> tasks = repository.findAll();
-        storage.saveTasks(tasks);
-    }
+  // タスクをストレージに保存
+  public void save() throws IOException {
+    List<Task> tasks = repository.findAll();
+    storage.saveTasks(tasks);
+  }
 
-    public void load() throws IOException {
-        List<Task> tasks = storage.loadTasks();
-        repository.replaceAll(tasks);
-    }
+  public void load() throws IOException {
+    List<Task> tasks = storage.loadTasks();
+    repository.replaceAll(tasks);
+  }
 
-    // TODO: 完了率の計算など
+  // TODO: 完了率の計算など
 }
