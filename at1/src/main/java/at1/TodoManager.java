4225eb7f81f65fc6bb8da13d1c3724d13ec1b26e
Kota-35 <ishikuro6.2@gmail.com>
2025-11-19 16:14:41 +0900

    feat: TodoManagerにタスク管理のためのリポジトリ、ストレージ、サービスを統合し、CommandHandlerを実行する機能を追加

--- at1/src/main/java/at1/TodoManager.java
+++ at1/src/main/java/at1/TodoManager.java
@@ -1,7 +1,19 @@
 package at1;
 
+import at1.domain.TaskRepository;
+import at1.infrastructure.FileTaskStorage;
+import at1.infrastructure.InMemoryTaskRepository;
+import at1.service.TaskService;
+import at1.ui.CommandHandler;
+
 public class TodoManager {
     public void run() {
 
+        TaskRepository repository = new InMemoryTaskRepository();
+        FileTaskStorage storage = new FileTaskStorage();
+        TaskService service = new TaskService(repository, storage);
+        CommandHandler handler = new CommandHandler(repository, service);
+
+        handler.run();
     }
 }
